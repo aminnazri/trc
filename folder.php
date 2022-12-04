4d59cf932142853954d3a9c2d62129fa30ced3c0
aminnazri <aminnurhazim@gmail.com>
2022-12-04 22:52:41 +0800

    add info button and search feature

--- folder.php
+++ folder.php
@@ -46,64 +46,83 @@ function get_sum($category){
 <!DOCTYPE html>
 <html lang="en">
 <style>
-    *{
-        text-decoration: none;
-    }
-    a:link { 
-        text-decoration: none; 
-        color: black;
-    }
-    .card:hover{
-        /* transform: scale(1.04); */
-        transition: all ease-in 0.20s;
-        transition: all ease-out 0.20s;
-        animation-duration: 1s;
-        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.1);
-    }
-    body{
-        padding:   0;
-    }
-    .middle{
-        display:flex;
-        margin-left:20px;
-        margin-right:20px;
-    }
+
 </style>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
+
+    <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Lily+Script+One&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="../../dist/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet"  href ='css/folder.css'/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Document</title>
 </head>
 <body class="">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <!-- <div id="products-cards-container"></div> -->
+
     <div class="middle">
+    
         <div class="container mt-5 ml-6  p-3 place " >
+            <div class="d-flex justify-content-center px-5"> 
+                <div class="search"> 
+                <input type="text" class="search-input" placeholder="Search..." name=""> 
+                    <a href="#" class="search-icon"> 
+                        <i class="fa fa-search"></i>
+                    </a>
+                </div>
+            </div>
             <div class="row gy-3 my-3" id="content">
+ 
                 <div class="col-sm-6 col-md-4 col-lg-3" >
-                    <a href="">
-                        <div class="card " style="width: 12rem;">
+                               <input class="filter" placeholder="filter" />
+                        <div class="card " style="width: 12rem;" data-string="amin">
                             <div class="image text-center text-bg-info rounded m-2">
                                 <svg style="width: 50%; height: auto;" class="card-image-top rounded " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M228.3 469.1L47.6 300.4c-4.2-3.9-8.2-8.1-11.9-12.4h87c22.6 0 43-13.6 51.7-34.5l10.5-25.2 49.3 109.5c3.8 8.5 12.1 14 21.4 14.1s17.8-5 22-13.3L320 253.7l1.7 3.4c9.5 19 28.9 31 50.1 31H476.3c-3.7 4.3-7.7 8.5-11.9 12.4L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9zM503.7 240h-132c-3 0-5.8-1.7-7.2-4.4l-23.2-46.3c-4.1-8.1-12.4-13.3-21.5-13.3s-17.4 5.1-21.5 13.3l-41.4 82.8L205.9 158.2c-3.9-8.7-12.7-14.3-22.2-14.1s-18.1 5.9-21.8 14.8l-31.8 76.3c-1.2 3-4.2 4.9-7.4 4.9H16c-2.6 0-5 .4-7.3 1.1C3 225.2 0 208.2 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141C165 36.5 211.4 51.4 244 84l12 12 12-12c32.6-32.6 79-47.5 124.6-39.9C461.5 55.6 512 115.2 512 185.1v5.8c0 16.9-2.8 33.5-8.3 49.1z"/></svg>
                             </div>
                             <div class="card-body m-6">
-                                <h5 class="card-title">
+                                <h5 class="title">
                                     Medical
                                 </h5>
+                                <i></i>
                                 <div class="text" >
                                     <small class="card-text1">Claimed : RM 800</small><br>
                                     <small class="card-text2">Balance : RM 70</small>
                                 </div>
                             </div>
                         </div>
-                    </a>
+        
+                    
+
+                  
+                        <div class="card " style="width: 12rem;"data-string="Jana B">
+                            <div class="image text-center text-bg-info rounded m-2">
+                                <svg style="width: 50%; height: auto;" class="card-image-top rounded " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M228.3 469.1L47.6 300.4c-4.2-3.9-8.2-8.1-11.9-12.4h87c22.6 0 43-13.6 51.7-34.5l10.5-25.2 49.3 109.5c3.8 8.5 12.1 14 21.4 14.1s17.8-5 22-13.3L320 253.7l1.7 3.4c9.5 19 28.9 31 50.1 31H476.3c-3.7 4.3-7.7 8.5-11.9 12.4L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9zM503.7 240h-132c-3 0-5.8-1.7-7.2-4.4l-23.2-46.3c-4.1-8.1-12.4-13.3-21.5-13.3s-17.4 5.1-21.5 13.3l-41.4 82.8L205.9 158.2c-3.9-8.7-12.7-14.3-22.2-14.1s-18.1 5.9-21.8 14.8l-31.8 76.3c-1.2 3-4.2 4.9-7.4 4.9H16c-2.6 0-5 .4-7.3 1.1C3 225.2 0 208.2 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141C165 36.5 211.4 51.4 244 84l12 12 12-12c32.6-32.6 79-47.5 124.6-39.9C461.5 55.6 512 115.2 512 185.1v5.8c0 16.9-2.8 33.5-8.3 49.1z"/></svg>
+                            </div>
+                            <div class="card-body m-6">
+                                <h5 class="title">
+                                    Medical
+                                </h5>
+                                <i></i>
+                                <div class="text" >
+                                    <small class="card-text1">Claimed : RM 800</small><br>
+                                    <small class="card-text2">Balance : RM 70</small>
+                                </div>
+                            </div>
+                        </div>
+               
                     
                 </div>
             </div>
@@ -114,10 +133,11 @@ function get_sum($category){
     
 
     
-
-<script type="text/javascript">
+<script src="js/tax_deduction_info.js"></script>
+<script type="">
 
     var lifestyle = "<?php echo $lifestyle?>";//8000
+    // const dc_parent_medical = "Include expenses to care for parents, i.e. through carer, for parents who suffer from diseases, physical or mental disabilities and need regular treatment certified by qualified medical practitioner. Include treatment and care at home, day care centres or home care centres.";
 
 
     console.log(lifestyle);
@@ -125,8 +145,8 @@ function get_sum($category){
     const valuesCards = [{
         image: 'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M228.3 469.1L47.6 300.4c-4.2-3.9-8.2-8.1-11.9-12.4h87c22.6 0 43-13.6 51.7-34.5l10.5-25.2 49.3 109.5c3.8 8.5 12.1 14 21.4 14.1s17.8-5 22-13.3L320 253.7l1.7 3.4c9.5 19 28.9 31 50.1 31H476.3c-3.7 4.3-7.7 8.5-11.9 12.4L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9zM503.7 240h-132c-3 0-5.8-1.7-7.2-4.4l-23.2-46.3c-4.1-8.1-12.4-13.3-21.5-13.3s-17.4 5.1-21.5 13.3l-41.4 82.8L205.9 158.2c-3.9-8.7-12.7-14.3-22.2-14.1s-18.1 5.9-21.8 14.8l-31.8 76.3c-1.2 3-4.2 4.9-7.4 4.9H16c-2.6 0-5 .4-7.3 1.1C3 225.2 0 208.2 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141C165 36.5 211.4 51.4 244 84l12 12 12-12c32.6-32.6 79-47.5 124.6-39.9C461.5 55.6 512 115.2 512 185.1v5.8c0 16.9-2.8 33.5-8.3 49.1z"',
         title: 'All',
-        text1: '00.00',
-        text2: '00.00',
+        text1: '',
+        text2: '',
         alt: 'image',
         link: 'all',
         bg_color: 'text-bg-info',
@@ -143,6 +163,7 @@ function get_sum($category){
     {
         image: 'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M228.3 469.1L47.6 300.4c-4.2-3.9-8.2-8.1-11.9-12.4h87c22.6 0 43-13.6 51.7-34.5l10.5-25.2 49.3 109.5c3.8 8.5 12.1 14 21.4 14.1s17.8-5 22-13.3L320 253.7l1.7 3.4c9.5 19 28.9 31 50.1 31H476.3c-3.7 4.3-7.7 8.5-11.9 12.4L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9zM503.7 240h-132c-3 0-5.8-1.7-7.2-4.4l-23.2-46.3c-4.1-8.1-12.4-13.3-21.5-13.3s-17.4 5.1-21.5 13.3l-41.4 82.8L205.9 158.2c-3.9-8.7-12.7-14.3-22.2-14.1s-18.1 5.9-21.8 14.8l-31.8 76.3c-1.2 3-4.2 4.9-7.4 4.9H16c-2.6 0-5 .4-7.3 1.1C3 225.2 0 208.2 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141C165 36.5 211.4 51.4 244 84l12 12 12-12c32.6-32.6 79-47.5 124.6-39.9C461.5 55.6 512 115.2 512 185.1v5.8c0 16.9-2.8 33.5-8.3 49.1z"',
         title: 'Parent Medical',
+        info: dc_parent_medical,
         text1: '<?php echo $parent_medical?>',
         text2: '30.00',
         alt: 'image',
@@ -158,15 +179,7 @@ function get_sum($category){
         link: '#',
         bg_color: 'text-bg-info',
     },
-    {
-        image: 'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M228.3 469.1L47.6 300.4c-4.2-3.9-8.2-8.1-11.9-12.4h87c22.6 0 43-13.6 51.7-34.5l10.5-25.2 49.3 109.5c3.8 8.5 12.1 14 21.4 14.1s17.8-5 22-13.3L320 253.7l1.7 3.4c9.5 19 28.9 31 50.1 31H476.3c-3.7 4.3-7.7 8.5-11.9 12.4L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9zM503.7 240h-132c-3 0-5.8-1.7-7.2-4.4l-23.2-46.3c-4.1-8.1-12.4-13.3-21.5-13.3s-17.4 5.1-21.5 13.3l-41.4 82.8L205.9 158.2c-3.9-8.7-12.7-14.3-22.2-14.1s-18.1 5.9-21.8 14.8l-31.8 76.3c-1.2 3-4.2 4.9-7.4 4.9H16c-2.6 0-5 .4-7.3 1.1C3 225.2 0 208.2 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141C165 36.5 211.4 51.4 244 84l12 12 12-12c32.6-32.6 79-47.5 124.6-39.9C461.5 55.6 512 115.2 512 185.1v5.8c0 16.9-2.8 33.5-8.3 49.1z"',
-        title: 'Supporting Equipment',
-        text1: '<?php echo $support_equipment?>',
-        text2: '30.00',
-        alt: 'image',
-        link: 's_e',
-        bg_color: 'text-bg-info',
-    },
+
     {
         image: 'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M228.3 469.1L47.6 300.4c-4.2-3.9-8.2-8.1-11.9-12.4h87c22.6 0 43-13.6 51.7-34.5l10.5-25.2 49.3 109.5c3.8 8.5 12.1 14 21.4 14.1s17.8-5 22-13.3L320 253.7l1.7 3.4c9.5 19 28.9 31 50.1 31H476.3c-3.7 4.3-7.7 8.5-11.9 12.4L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9zM503.7 240h-132c-3 0-5.8-1.7-7.2-4.4l-23.2-46.3c-4.1-8.1-12.4-13.3-21.5-13.3s-17.4 5.1-21.5 13.3l-41.4 82.8L205.9 158.2c-3.9-8.7-12.7-14.3-22.2-14.1s-18.1 5.9-21.8 14.8l-31.8 76.3c-1.2 3-4.2 4.9-7.4 4.9H16c-2.6 0-5 .4-7.3 1.1C3 225.2 0 208.2 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141C165 36.5 211.4 51.4 244 84l12 12 12-12c32.6-32.6 79-47.5 124.6-39.9C461.5 55.6 512 115.2 512 185.1v5.8c0 16.9-2.8 33.5-8.3 49.1z"',
         title: 'Medical Expenses',
@@ -220,21 +233,39 @@ function get_sum($category){
         alt: 'image',
         link: '#',
         bg_color: 'text-bg-info',
+    },
+    {
+        image: 'xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M228.3 469.1L47.6 300.4c-4.2-3.9-8.2-8.1-11.9-12.4h87c22.6 0 43-13.6 51.7-34.5l10.5-25.2 49.3 109.5c3.8 8.5 12.1 14 21.4 14.1s17.8-5 22-13.3L320 253.7l1.7 3.4c9.5 19 28.9 31 50.1 31H476.3c-3.7 4.3-7.7 8.5-11.9 12.4L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9zM503.7 240h-132c-3 0-5.8-1.7-7.2-4.4l-23.2-46.3c-4.1-8.1-12.4-13.3-21.5-13.3s-17.4 5.1-21.5 13.3l-41.4 82.8L205.9 158.2c-3.9-8.7-12.7-14.3-22.2-14.1s-18.1 5.9-21.8 14.8l-31.8 76.3c-1.2 3-4.2 4.9-7.4 4.9H16c-2.6 0-5 .4-7.3 1.1C3 225.2 0 208.2 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141C165 36.5 211.4 51.4 244 84l12 12 12-12c32.6-32.6 79-47.5 124.6-39.9C461.5 55.6 512 115.2 512 185.1v5.8c0 16.9-2.8 33.5-8.3 49.1z"',
+        title: 'Supporting Equipment',
+        text1: '<?php echo $support_equipment?>',
+        text2: '30.00',
+        alt: 'image',
+        link: 's_e',
+        bg_color: 'text-bg-info',
     }
 ]
 
     function returnCards(valuesCards) {
         return  valuesCards.map(valuesCard => `
         
-        <div class="col-sm-6 col-md-3 col-lg-3" >
 
-            <div class="card">
+
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3" >
+        
+
+            <div class="card" data-string="${valuesCard.title}">
                 <a href = "images.php?f=${valuesCard.link}" >
                     <div class="image text-center ${valuesCard.bg_color} rounded m-2">
                             <svg style="width: 50%; height: auto;" class="card-image-top rounded " ${valuesCard.image}"></svg>
                         </div>
                 <div class="card-body budi">
-                    <h5 class="card-title">${valuesCard.title}</h5>
+                    <div class = "d-flex justify-content-between align-content-center">
+                        <h5 class="card-title">${valuesCard.title}</h5>
+                        <i href="#" data-bs-toggle="tooltip" data-bs-placement="left"  title="${valuesCard.info}"><span><iconify-icon icon="material-symbols:info-outline-rounded"></span></iconify-icon></i>
+
+                    </div>
                     <div class = "text">
                     
                         <small class="">Claimed : RM ${valuesCard.text1}</small>
@@ -256,8 +287,45 @@ function get_sum($category){
     $(document).ready(function () {
     		$(".folders").addClass("active");
     });
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    const useStyles = makeStyles(theme =>
+        createStyles({
+            tooltip: {
+                maxWidth: '200px',
+            },
+            tooltipPlacementTop: {
+                margin: '0px 0',
+            },
+            tooltipPlacementBottom: {
+                margin: '0px 0',
+            },
+        })
+    )
+
+
     document.title = 'Folder';
 
 </script>
+<script>
+
+
+$(".search-input").on("keyup", function() {
+  var input = $(this).val().toUpperCase();
+
+  $(".card").each(function() {
+    if ($(this).data("string").toUpperCase().indexOf(input) < 0) {
+      $(this).hide();
+    } else {
+      $(this).show();
+    }
+  })
+});
+</script>
+
 </body>
 </html>
